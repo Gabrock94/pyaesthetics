2b1a651de6f0eac7c224029091df06ced2900c72
Unknown <gack94@gmail.com>
2018-04-16 12:05:58 +0200

    Structure + QuadTree Decomposition
    - Added structure
    - Initialized setup.py
    - Added simple image
    - Implemented a QuadTree decomposition algorithm for complexity analysis
    - Added function for visual representation of QuadTree Decomposition

---
+++ prettywebsite/__init__.py
@@ -0,0 +1,6 @@
+from . import quadTreeDecomposition
+
+# This is used to print out the package version
+__version__ = '0.0.1' #Version Control
+
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(name='prettywebsite',
+    version='0.0.1',
+    description='Website aesthetic analysis in Python',
+    long_description="A simple python package for aesthetic analysis of web pages.",
+    url='https://github.com/Gabrock94/PrettyWebsite',
+    download_url='',
+    author='Giulio Gabrieli',
+    author_email='gack94@gmail.com',
+    license='GPL',
+    packages=['prettywebsite'],      
+    install_requires=[
+        'numpy',
+        'cv2',
+        'scipy',
+        'matplotlib'
+    ],
+    keywords = ["Website","Analysis","Aesthetic"],
+    classifiers = [ 
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved ::  GNU General Public License v3 (GPLv3)',
+        
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    zip_safe=False)
