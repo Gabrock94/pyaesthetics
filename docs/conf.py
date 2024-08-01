35e3805197080cf46c1fdf493038c0fcafd1eeaa
Giulio <gack94@gmail.com>
2024-08-01 12:28:29 +0200

    readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Giulio Gabrieli'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.7'
+release = '0.0.8'
 
 
 # -- General configuration ---------------------------------------------------
