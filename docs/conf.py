161d78cf40aad713105be0f87461ef27dc1ba7a3
Giulio <gack94@gmail.com>
2024-08-03 18:50:43 +0200

    templates

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,11 @@ import os
 import sys
 import sphinx_autosummary_accessors
 
+
+from unittest.mock import Mock
+
+sys.modules['face_recognition'] = Mock()
+
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../pyaesthetics'))
 
