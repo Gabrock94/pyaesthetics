b5c782a369a0f006b3efc18484e35b26a8a898d5
Giulio <gack94@gmail.com>
2024-08-03 17:03:53 +0200

    weird

--- pyaesthetics/__init__.py
+++ pyaesthetics/__init__.py
@@ -1,15 +1,17 @@
-from . import quadtreedecomposition
-from . import spacebaseddecomposition
-from . import colorfulness
-from . import brightness
-from . import symmetry
-from . import facedetection
 from . import analysis
+from . import brightness
+from . import colordetection
+from . import colorfulness
 from . import contrast
-from . import saturation
-# from . import selfsimilarity
+from . import facedetection
 from . import linesdetection
+from . import quadtreedecomposition
+from . import saturation
+from . import selfsimilarity
+from . import spacebaseddecomposition
+from . import symmetry
 from . import utils
+from . import visualcomplexity
 
 # This is used to print out the package version
 __version__ = '0.0.8.1' #Version Control
