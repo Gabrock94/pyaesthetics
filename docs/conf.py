68550be721300805fe3fa7d38609871dadabc355
Giulio Gabrieli <gack94@gmail.com>
2026-03-18 16:37:50 +0000

    Updates package version and Python support
    Increments the package version to 0.0.8.10 across all relevant files.
    Expands declared Python compatibility to include versions 3.13 and 3.14.

--- docs/conf.py
+++ docs/conf.py
@@ -34,9 +34,9 @@ copyright = '2024, Giulio Gabrieli'
 author = 'Giulio Gabrieli'
 
 # The short X.Y version
-version = '0.0.8.9'
+version = '0.0.8.10'
 # The full version, including alpha/beta/rc tags
-release = '0.0.8.9'
+release = '0.0.8.10'
 
 
 # -- General configuration ---------------------------------------------------
